2fb757331e139425bcde51e3760f6b06375e1691
Eskindir <eskindiraleb@yahoo.com>
2021-04-21 15:35:47 -0500

    hello page

--- LiftoffProjeEvent/event/src/main/java/EventProject/example/event/Hello.java
+++ LiftoffProjeEvent/event/src/main/java/EventProject/example/event/Hello.java
@@ -1,4 +1,9 @@
 package EventProject.example.event;
 
+import EventProject.example.event.controllers.HomeController;
+
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println(HomeController.hello());
+    }
 }
--- LiftoffProjeEvent/event/src/main/java/EventProject/example/event/controllers/HomeController.java
+++ LiftoffProjeEvent/event/src/main/java/EventProject/example/event/controllers/HomeController.java
@@ -1,4 +1,15 @@
 package EventProject.example.event.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+    @GetMapping
+    @ResponseBody
+    public static String hello(){
+        return "Hello There!";
+    }
 }
